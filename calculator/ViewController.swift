e99973ff635c66d8b0c749c37a37e065c6c67964
dasha0501 <63141331+dasha0501@users.noreply.github.com>
2022-03-30 15:51:42 +0900

    Add files via upload

---
+++ calculator/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Гость on 30.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var buttonAC: UIButton!
+    @IBOutlet weak var buttonZero: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var buttonChangeResultLabel: UIStepper!
+  
+    var numberOne = ""
+    var numberTwo = ""
+    var operand = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+}
+
